fae8852abd6ccb1f2c73912eed74c3eedc921111
Kevin <kevincyap@steamdeck.com>
2024-11-08 18:58:26 -0800

    init

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,15 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
+#define MIRYOKU_CLIPBOARD_WIN
+#define KEVIN_ALPHA_QWERTY
+#define KEVIN_EXTRA_COLEMAKDH
+#define KEVIN_TAP_DVORAK
+#define KEVIN_NAV
+#define KEVIN_NUM
+#define KEVIN_SYM
+#define KEVIN_MOUSE
+#define KEVIN_BUTTON
+#define KEVIN_COMBOS
 
+#define CONFIG_ZMK_BLE_THREAD_PRIORITY 5
+#define MIRYOKU_KLUDGE_MOUSEKEYSPR
