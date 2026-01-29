62da8b62d1e90fa9e4fa0a5dda1b7c11e5cb5a20
kevinyap <kevinyap+odspmdb@microsoft.com>
2026-01-29 10:19:28 -0800

    add artsey

--- miryoku/miryoku_babel/kevin_layer_alternatives.h
+++ miryoku/miryoku_babel/kevin_layer_alternatives.h
@@ -66,3 +66,9 @@ U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,                &u_out_tog,        &kp C_PREV,        &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_NEXT,        \
 U_NA,              &kp RALT,          &u_to_U_FUN,       &u_to_U_MEDIA,     U_NA,                &bt BT_CLR,        &u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,       \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,                &kp C_STOP,        &kp C_PP,          &kp C_MUTE,        U_NP,              U_NP
+
+#define KEVIN_MAP_ARTSEY \
+&kp S,             &kp T,             &kp R,             &kp A,             U_NA,                 U_NA,                 &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,                \
+&kp O,             &kp I,             &kp Y,             &kp E,             U_NA,                 U_NA,                 U_NA,             U_NA,             U_NA,             U_NA,                \
+U_NA,             U_NA,               U_NA,              U_NA,              &u_to_U_BASE,         U_NA,                 U_NA,             U_NA,             U_NA,             U_NA,                \
+U_NP,             U_NP,               U_LT(U_SYM, ESC),  U_LT(U_NAV, SPACE),U_LT(U_MOUSE, RET),   U_NA,    U_NA,             U_NA,  U_NP,              U_NP
